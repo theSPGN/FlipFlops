24fc9ae76df26342445a3f11ffec6e9cc91ecd20
spgn <spgn1226@gmail.com>
2024-01-17 18:42:15 +0100

    A bunch of changes to improve functionality of AndGate so it takes only Boolean type of variables. Also there is a new feature that you can initialize AndGate and just call the output or you can call object to get other result.

--- and_gate.py
+++ and_gate.py
@@ -1,15 +1,29 @@
 class AndGate:
-    def __init__(self, *args: tuple[bool]) -> None:
+    def __init__(self, *args: bool) -> None:
+        if not all(isinstance(arg, bool) for arg in args):
+            raise ValueError("All arguments must be booleans")
+
         self.args = args
 
-    def __call__(self, *args) -> bool:
-        return all(args) if len(args) > 0 else False
+    def __call__(self, *args: bool) -> bool:
+        if not all(isinstance(arg, bool) for arg in args):
+            raise ValueError("All arguments must be booleans")
+
+        if len(args):
+            return all(args) if len(args) > 0 else False
+        else:
+            return all(self.args) if len(self.args) > 0 else False
 
     def __str__(self) -> str:
         return "AndGate"
 
 
-# callback:
-# obj = AndGate()
-# print(obj, type(obj))
+# callback instruction:
+
+# obj = AndGate(True, True, True)
+# print("---")
+# print(obj)
+# print(type(obj))
+# print(obj())
+# print(type(obj()))
 # print(obj(True, True, False, True))
