8a85b57ba5939615f5e13b763c046ae01bb07959
spgn <spgn1226@gmail.com>
2024-01-17 19:18:13 +0100

    NOT GATE IMPLEMENTATION

--- gates.py
+++ gates.py
@@ -1,3 +1,12 @@
+"""
+
+It's about electronics,
+not about Mr. Bill...
+;)
+
+"""
+
+
 class AndGate:
     """
     callback instruction for AndGate:
@@ -31,7 +40,7 @@ class AndGate:
 
 class OrGate:
     """
-    callback instruction for AndGate:
+    callback instruction for OrGate:
     obj = OrGate(False, False, False)
     print("---")
     print(obj)
@@ -58,3 +67,34 @@ class OrGate:
 
     def __str__(self) -> str:
         return "OrGate"
+
+
+class NotGate:
+    """
+    callback instruction for NotGate:
+    obj = NotGate(False, False, False)
+    print("---")
+    print(obj)
+    print(type(obj))
+    print(obj())
+    print(type(obj()))
+    print(obj(True, True, False, True))
+    """
+
+    def __init__(self, *args: bool) -> None:
+        if not all(isinstance(arg, bool) for arg in args):
+            raise ValueError("All arguments must be booleans")
+
+        self.args = args
+
+    def __call__(self, *args: bool) -> tuple[bool]:
+        if not all(isinstance(arg, bool) for arg in args):
+            raise ValueError("All arguments must be booleans")
+
+        if len(args):
+            return tuple(not arg for arg in args)
+        else:
+            return tuple(not arg for arg in self.args)
+
+    def __str__(self) -> str:
+        return "NotGate"
