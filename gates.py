6fd5a3650d179de5bb4df348455361f1b5c4bedf
spgn <spgn1226@gmail.com>
2024-01-17 19:07:01 +0100

    "OR" gate implementation

--- gates.py
+++ gates.py
@@ -1,4 +1,15 @@
 class AndGate:
+    """
+    callback instruction for AndGate:
+    obj = AndGate(True, True, True)
+    print("---")
+    print(obj)
+    print(type(obj))
+    print(obj())
+    print(type(obj()))
+    print(obj(True, True, False, True))
+    """
+
     def __init__(self, *args: bool) -> None:
         if not all(isinstance(arg, bool) for arg in args):
             raise ValueError("All arguments must be booleans")
@@ -18,12 +29,32 @@ class AndGate:
         return "AndGate"
 
 
-# callback instruction for AndGate:
+class OrGate:
+    """
+    callback instruction for AndGate:
+    obj = OrGate(False, False, False)
+    print("---")
+    print(obj)
+    print(type(obj))
+    print(obj())
+    print(type(obj()))
+    print(obj(True, True, False, True))
+    """
 
-# obj = AndGate(True, True, True)
-# print("---")
-# print(obj)
-# print(type(obj))
-# print(obj())
-# print(type(obj()))
-# print(obj(True, True, False, True))
+    def __init__(self, *args: bool) -> None:
+        if not all(isinstance(arg, bool) for arg in args):
+            raise ValueError("All arguments must be booleans")
+
+        self.args = args
+
+    def __call__(self, *args: bool) -> bool:
+        if not all(isinstance(arg, bool) for arg in args):
+            raise ValueError("All arguments must be booleans")
+
+        if len(args):
+            return any(args) if len(args) > 0 else False
+        else:
+            return any(self.args) if len(self.args) > 0 else False
+
+    def __str__(self) -> str:
+        return "OrGate"
