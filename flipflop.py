261c1386309fdb9e6894213bafdd9b29dc5f35c4
spgn <spgn1226@gmail.com>
2024-01-22 22:00:19 +0100

    Async / sync Rs flipflop edition

--- flipflop.py
+++ flipflop.py
@@ -1,7 +1,38 @@
+from gates import NandGate as nand
+
+
+# All the flip flops are made with nands
 class RS:
-    def __init__(self, r: bool = False, s: bool = False):
-        if not isinstance(r, bool) or not isinstance(s, bool):
+    def __init__(
+        self, r: bool = False, s: bool = False, clk: bool = False, type: str = "async"
+    ):
+        if (
+            not isinstance(r, bool)
+            or not isinstance(s, bool)
+            or not isinstance(clk, bool)
+        ):
             raise ValueError("All arguments must be booleans")
+
+        if type is not "async" and type is not "sync":
+            raise ValueError('Value of type has to be "sync" or "async"')
+
         self.r = r
         self.s = s
-        self.q = float("nan")
+        self.q = None
+        self.clk = clk
+        self.type = type
+
+    def __call__(self, r: bool = None, s: bool = None) -> bool:
+        if r is not None:
+            self.r = r
+        if s is not None:
+            self.s = s
+
+        return self.q
+
+    def __str__(self) -> str:
+        return "RSAsync"
+
+
+k = RS(True, False)
+print(k())
