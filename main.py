1b6692fd6bd69fd8d6badbff94c88bf4e9d50ace
theSPGN <spgn@gmail.com>
2024-02-04 23:29:16 +0100

    finallizing project

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import flipflop as ff
 
 if __name__ == "__main__":
 
+
     q = ff.RS(r=0, s=0, clk=0, default_q=True, rs_type="sync")
     print("\nRS-sync flipflop:")
     for clk in range(2):
@@ -10,6 +11,7 @@ if __name__ == "__main__":
                 output = q(r=r, s=s, clk=clk)
                 print(f"r={r}, s={s}, clk={clk} q={int(output)}")
 
+
     q = ff.RS(r=0, s=0, default_q=False, rs_type="async")
     print("\nRS-async flipflop:")
     for r in range(2):
@@ -17,12 +19,14 @@ if __name__ == "__main__":
             output = q(r=r, s=s)
             print(f"r={r}, s={s}, q={int(output)}")
 
+
     q = ff.D(d=0, clk=0, default_q=True)
     print("\nD flipflop:")
     for clk in range(2):
         for d in range(2):
             output = q(d=d, clk=clk)
             print(f"d={d}, clk={clk}, q={int(output)}")
+
 
     q = ff.JK(j=0, k=0, clk=0, default_q=False)
     print("\nJK flipflop:")
